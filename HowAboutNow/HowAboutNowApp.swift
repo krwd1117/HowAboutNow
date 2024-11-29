85b2fa8451abf0a93acf1f57865313860ea6cbbd
김정완 <krwd1117@gmail.com>
2024-11-29 22:56:49 +0900

    Project 생성

---
+++ HowAboutNow/HowAboutNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowAboutNowApp.swift
+//  HowAboutNow
+//
+//  Created by Jeongwan Kim on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct HowAboutNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
